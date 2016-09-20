a4641a736e47aa241d1a43fbf5cf45b5c1dae334
brapifra <brapifra@hotmail.com>
2016-09-20 19:49:59 +0200

    Huge changes: Windows support, auto open browser

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Host/Serve current workspace (or subfolder) with PHP.
 
 ## Features
 
-You can execute it with ctrl+shift+P and searching for "Serve Project with PHP" or you can just click on the editor's button (right superior corner).
+You can execute it with:
+-ctrl+shift+P and searching for "Serve Project with PHP"
+-Clicking on the editor's button (right superior corner)
+-Right-clicking on the editor.
 
+To stop the server just search for "Stop PHP Server" or right-click on the editor.
+
+Now with Windows support, auto open localhost in browser.
 
 ## Requirements
 
@@ -16,9 +22,16 @@ This extension contributes the following settings:
 
 * `phpserver.port`: Localhost port where server will be hosted (Default: 3000)
 * `phpserver.relativeurl`: URL relative to workspace path, in case you want only host a subfolder of workspace (Default: "")
+* `phpserver.browser`: Browser's name that will be opened when server starts, if value='' then no browser will be opened (Default: "firefox", Options: "chrome, firefox, ''").
 
 
 ## Release Notes
+### 1.1.0
+Code rewritten (almost):
++Windows support.
++Auto open localhost in browser (optional).
++Added context menu commands.
++Fixed problem with relative url.
 ### 1.0.0
 Initial release of PHP Server
 -----------------------------------------------------------------------------------------------------------
--- extension.js
+++ extension.js
@@ -1,13 +1,65 @@
 var vscode = require('vscode');
+var os = require('os');
+var config = vscode.workspace.getConfiguration('phpserver');
+var browser = config.get("browser");
+var relativeurl = config.get("relativeurl");
+var port = config.get("port");
+var platform = os.platform();
+switch (browser) {
+    case "firefox":
+        if (platform == 'win32') {
+            browser = "start " + browser;
+        }
+        break;
+    case "chrome":
+        if (platform == 'linux' || platform == 'darwin') {
+            browser = "google-chrome";
+        } else if (platform == 'win32') {
+            browser = "start " + browser;
+        }
+        break;
+}
+var serverterminal;
+var browserterminal;
 function activate(context) {
+    var out = vscode.window.createOutputChannel("PHP Server");
     context.subscriptions.push(vscode.commands.registerCommand('extension.serveProject', function () {
-        var config=vscode.workspace.getConfiguration('phpserver');
-        var terminal=vscode.window.createTerminal("PHP Server");
-        terminal.sendText("php -S localhost:"+config.get("port")+" -t "+vscode.workspace.rootPath+"/"+config.get("relativeurl"));
+        if (serverterminal) {
+            vscode.window.showErrorMessage('Server already running!');
+            return;
+        }
+        out.clear();
+        out.show();
+        var args = ['-S', 'localhost:' + port];
+        if (relativeurl != "") {
+            args.push("-t");
+            args.push(relativeurl);
+        }
+        serverterminal = require('child_process').spawn('php', args, { cwd: vscode.workspace.rootPath });
+        serverterminal.stdout.on('data', (data) => {
+            out.appendLine(`${data}`);
+        });
+        serverterminal.stderr.on('data', (data) => {
+            out.appendLine(`${data}`);
+        });
+        serverterminal.on('close', (code) => {
+            vscode.window.showInformationMessage('Server Stopped');
+            out.hide();
+        });
         vscode.window.showInformationMessage('Serving Project');
     }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.stopServer', function () {
+        deactivate();
+    }));
+    if (browser != "") {
+        browserterminal = require('child_process').spawn(browser, ['http://localhost:' + port]);
+    }
 }
 exports.activate = activate;
 function deactivate() {
+    if (serverterminal) {
+        serverterminal.kill();
+        serverterminal = undefined;
+    }
 }
 exports.deactivate = deactivate;
