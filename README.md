ebdf76396618915039f2ddb84af1ced1df2d0143
Brais Piñeiro <brapifra@gmail.com>
2017-08-21 19:44:04 +0200

    Version 2.0.0, see changelog

--- README.md
+++ README.md
@@ -19,15 +19,17 @@ PHP installed.
 This extension contributes the following settings:
 
 * `phpserver.port`: Server Port Number (Default: 3000)
-* `phpserver.path`: Project path (In case that you want to serve a different folder than workspace, Default: "")
-* `phpserver.browser`: Select the browser that will open localhost (Default: "firefox", Options: "chrome, firefox, ''").
-* `phpserver.external`: The server is accessible from any interface of your machine (remote machines).
+* `phpserver.relativePath`: Path relative to project (In case that you want to serve a different folder than workspace, Default: "")
+* `phpserver.browser`: Select the browser that will open localhost (Default: "firefox", Options: "chrome, firefox, edge, ''").
+* `phpserver.ip`: Server IP ('localhost', '0.0.0.0', ...).
 
 
 ## Release Notes
 ### 2.0.0
-* External option added
-* RelativeUrl option removed (Now is "path");
+* **Windows console output fixed**
+* IP option added
+* RelativeUrl option removed (Now is "relativePath");
+* Microsoft edge browser added
 ### 1.1.0
 Code rewritten (almost):<br>
 * Windows support.
@@ -39,12 +41,11 @@ Initial release of PHP Server
 
 ## Known bugs
 
-#### No console output in Windows
+#### ~~No console output in Windows~~
 -----------------------------------------------------------------------------------------------------------
 
 ## For more information
 
-* [Programo Luego Existo](http://programoluegoexisto.com)
 * [GitHub](http://github.com/brapifra)
 
 **Enjoy!**
--- src/extension.js
+++ src/extension.js
@@ -1,73 +1,91 @@
-var vscode = require('vscode');
-var os = require('os');
-var config = vscode.workspace.getConfiguration('phpserver');
-var browser = config.get("browser");
-var relativeurl = config.get("relativeurl");
-var port = config.get("port");
-var external = config.get("external");
-var platform = os.platform();
-switch (browser) {
-    case "firefox":
-     if (platform == 'linux' || platform == 'darwin') {
-            browser = browser+" http:/localhost:"+port;
-        }else if (platform == 'win32') {
-            browser= "start "+browser+" http:/localhost:"+port;
-        }
-        break;
-    case "chrome":
-        if (platform == 'linux' || platform == 'darwin') {
-            browser = "google-chrome http:/localhost:"+port;
-        } else if (platform == 'win32') {
-            browser= "start "+browser+" http:/localhost:"+port;
-        }
-        break;
-}
-var serverterminal;
-var browserterminal;
+const vscode = require("vscode");
+const child = require("child_process");
+const platform = require("os").platform();
+const config = vscode.workspace.getConfiguration("phpserver");
+const relativePath = config.get("relativePath");
+const port = config.get("port");
+const ip = config.get("ip");
+let browser = config.get("browser");
+let serverterminal;
+let browserterminal;
+
 function activate(context) {
-    var out = vscode.window.createOutputChannel("PHP Server");
-    context.subscriptions.push(vscode.commands.registerCommand('extension.serveProject', function () {
-        if (serverterminal) {
-            vscode.window.showErrorMessage('Server already running!');
-            return;
+  checkBrowser();
+  const out = vscode.window.createOutputChannel("PHP Server");
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.serveProject", function() {
+      if (serverterminal) {
+        vscode.window.showErrorMessage("Server is already running!");
+        return;
+      }
+      out.clear();
+      out.show();
+      const args = ["-S", `${ip}:${port}`];
+      if (relativePath != "") {
+        args.push("-t");
+        args.push(relativePath);
+      }
+      if (platform == "win32") {
+        args.push(`${context.extensionPath}\\src\\logger.php`);
+        if (relativePath != "") {
+          process.env.PHP_SERVER_RELATIVE_PATH = relativePath;
         }
-        out.clear();
-        out.show();
-        var args = ['-S', 'localhost:' + port];
+      }
 
-        if (external) {
-            args = ['-S', '0.0.0.0:' + port];
-        }
-
-        if (relativeurl != "") {
-            args.push("-t");
-            args.push(relativeurl);
-        }
-        serverterminal = require('child_process').spawn('php', args, { cwd: vscode.workspace.rootPath });
-        serverterminal.stdout.on('data', function(data) {
-            out.appendLine(data.toString());
-        });
-        serverterminal.stderr.on('data', function(data){
-            out.appendLine(data.toString());
-        });
-        serverterminal.on('close', function(code){
-            vscode.window.showInformationMessage('Server Stopped');
-            out.hide();
-        });
-        vscode.window.showInformationMessage('Serving Project');
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('extension.stopServer', function () {
+      serverterminal = child.spawn("php", args, {
+        cwd: vscode.workspace.rootPath
+      });
+      serverterminal.stdout.on("data", function(data) {
+        out.appendLine(data.toString());
+      });
+      serverterminal.stderr.on("data", function(data) {
+        out.appendLine(data.toString());
+      });
+      serverterminal.on("close", function(code) {
+        vscode.window.showInformationMessage("Server Stopped");
         deactivate();
-    }));
-    if (browser != "") {
-        browserterminal=require('child_process').exec(browser);
-    }
+        out.hide();
+      });
+      vscode.window.showInformationMessage("Serving Project");
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.stopServer", deactivate)
+  );
+  if (browser !== "") {
+    browserterminal = child.exec(browser);
+  }
 }
 exports.activate = activate;
 function deactivate() {
-    if (serverterminal) {
-        serverterminal.kill();
-        serverterminal = undefined;
-    }
+  if (serverterminal) {
+    serverterminal.kill();
+    serverterminal = undefined;
+  }
 }
 exports.deactivate = deactivate;
+function checkBrowser() {
+  switch (browser) {
+    case "firefox":
+      if (platform == "linux" || platform == "darwin") {
+        browser = `${browser} http://${ip}:${port}`;
+      } else if (platform == "win32") {
+        browser = `start ${browser} http://${ip}:${port}`;
+      }
+      break;
+    case "chrome":
+      if (platform == "linux" || platform == "darwin") {
+        browser = `google-chrome http://${ip}:${port}`;
+      } else if (platform == "win32") {
+        browser = `start ${browser} http://${ip}:${port}`;
+      }
+      break;
+    case "edge":
+      if (platform == "linux" || platform == "darwin") {
+        browser = "";
+      } else if (platform == "win32") {
+        browser = `start microsoft-edge:http://${ip}:${port}`;
+      }
+      break;
+  }
+}
