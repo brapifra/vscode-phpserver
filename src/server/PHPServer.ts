6b59c07bc6288d1dd2cfae6cdd3beb900aefde02
Brais Piñeiro <brapifra@gmail.com>
2020-08-01 17:11:29 +0200

    Add PHPServer class

---
+++ src/server/PHPServer.ts
@@ -0,0 +1,80 @@
+import { spawn, ChildProcess } from 'child_process';
+
+type ServerEvent = 'data' | 'error' | 'close';
+
+type ServerEventHandler = (data: string) => void;
+
+export interface PHPServerConfig {
+  host: string;
+  port: number;
+  rootPath: string;
+  relativePath: string;
+  phpExecutablePath: string;
+  phpRouterPath?: string;
+  phpConfigPath?: string;
+}
+
+export default class PHPServer {
+  private process?: ChildProcess;
+  private listeners: Array<[ServerEvent, ServerEventHandler]> = [];
+  private config?: PHPServerConfig;
+
+  constructor() {}
+
+  on(event: ServerEvent, handler: ServerEventHandler): void {
+    this.listeners.push([event, handler]);
+  }
+
+  start(config: PHPServerConfig): void {
+    this.config = config;
+
+    this.process = spawn(
+      config.phpExecutablePath,
+      this.getProcessArgs(config),
+      {
+        cwd: config.rootPath,
+      }
+    );
+
+    this.setupProcessListeners();
+  }
+
+  stop(): void {
+    this.process?.kill();
+    this.process = undefined;
+  }
+
+  isRunning() {
+    return !!this.process;
+  }
+
+  getCurrentConfig(): PHPServerConfig | undefined {
+    return this.config;
+  }
+
+  private getProcessArgs(config: PHPServerConfig): string[] {
+    const args: string[] = [
+      '-S',
+      `${config.host}:${config.port}`,
+      '-t',
+      config.relativePath,
+    ];
+
+    if (config.phpConfigPath) {
+      args.push('-c');
+      args.push(config.phpConfigPath);
+    }
+
+    if (config.phpRouterPath) {
+      args.push(config.phpRouterPath);
+    }
+
+    return args;
+  }
+
+  private setupProcessListeners() {
+    this.listeners.forEach(([event, eventHandler]) => {
+      this.process?.on(event, eventHandler);
+    });
+  }
+}
