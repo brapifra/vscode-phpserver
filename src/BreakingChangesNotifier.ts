276974b38a7b61991b4d32c25803e33f37031283
Brais Piñeiro <brapifra@gmail.com>
2020-08-17 23:41:03 +0200

    Use correct extension identifier

--- src/BreakingChangesNotifier.ts
+++ src/BreakingChangesNotifier.ts
@@ -1,6 +1,12 @@
 import * as vscode from 'vscode';
 import Messages from './Messages';
 
+const EXTENSION_ID = 'brapifra.phpserver';
+
+enum GlobalStateField {
+  BREAKING_CHANGES_DISMISSED = 'BREAKING_CHANGES_DISMISSED',
+}
+
 export default class BreakingChangesNotifier {
   constructor(public globalState: vscode.Memento) {}
 
@@ -13,20 +19,24 @@ export default class BreakingChangesNotifier {
         .showInformationMessage(Messages.BROWSER_BREAKING_CHANGES, Messages.OK)
         .then((result) => {
           if (result === Messages.OK)
-            this.globalState.update('BREAKING_CHANGES_DISMISSED', true);
+            this.globalState.update(
+              GlobalStateField.BREAKING_CHANGES_DISMISSED,
+              true
+            );
         });
     }
   }
 
   private doesCurrentVersionContainBreakingChanges(): boolean {
-    const extension = vscode.extensions.getExtension(
-      'brapifra.vscode-phpserver'
-    );
+    const extension = vscode.extensions.getExtension(EXTENSION_ID);
 
     return extension?.packageJSON.version >= '3.0.0';
   }
 
   private wasBreakingChangesWarningDismissed() {
-    return this.globalState.get('BREAKING_CHANGES_DISMISSED', false);
+    return this.globalState.get(
+      GlobalStateField.BREAKING_CHANGES_DISMISSED,
+      false
+    );
   }
 }
